5ab36327d4165a760aa4aef1415bdcce30b42c84
DenysMkl <den.mikliaiev@gmail.com>
2023-01-28 00:19:42 +0300

    new changes

--- prac.go
+++ prac.go
@@ -4,32 +4,40 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strconv"
 )
 
 func main() {
-
-	var urls []string = []string{}
-	for i:= 1; i <=100; i++{
-		urls = append(urls, "https://jsonplaceholder.typicode.com/posts/"+strconv.Itoa(i))	
+	urls := []string{}
+	for i:= 1; i <= 10; i++{
+		urls = append(urls,  "https://jsonplaceholder.typicode.com/posts/"+strconv.Itoa(i))
 	}
+
+	err := os.MkdirAll("storage/posts", 0750)
+	if err != nil{
+		fmt.Println("error")
+	}
+
+
 	channel := make(chan string)
 	
-	
 	for _, url := range urls{
-		
 		go func (url string, channel chan string)  {
 			channel <- Get_data(url)
-
 		}(url, channel)
-
+	
 	}
+
+	var counter int = 1
 	for i := range channel{
-		fmt.Println(i)
-	}	
-
+		file, _ := os.Create("storage/posts/"+strconv.Itoa(counter)+".txt")
+		counter+=1
+		file.WriteString(i)
+		file.Close()
+	}
+	
 }
-
 
 func Get_data(link string) string{
 	
